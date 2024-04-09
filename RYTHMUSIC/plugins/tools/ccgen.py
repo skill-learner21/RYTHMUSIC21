7d85503cdd1757fb47b256c371d759bd380a1b16
husain <danishkhan77327@gmail.com>
2024-04-09 22:21:08 +0530

    uploded files

---
+++ RYTHMUSIC/plugins/tools/ccgen.py
@@ -0,0 +1,38 @@
+from ... import *
+from pyrogram import *
+from pyrogram.types import *
+
+
+@app.on_message(filters.command(["gen", "ccgen"], [".", "!", "/"]))
+async def gen_cc(client, message):
+    if len(message.command) < 2:
+        return await message.reply_text(
+            "**๏ ᴘʟᴇᴀsᴇ ɢɪᴠᴇ ᴍᴇ ᴀ ʙɪɴ ᴛᴏ\n๏ ɢᴇɴᴇʀᴀᴛᴇ ᴄᴄ ...**"
+        )
+    try:
+        await message.delete()
+    except:
+        pass
+    aux = await message.reply_text("**✦ ɢᴇɴᴇʀᴀᴛɪɴɢ ...**")
+    bin = message.text.split(None, 1)[1]
+    if len(bin) < 6:
+        return await aux.edit("**❌ ᴡʀᴏɴɢ ʙɪɴ❗...**")
+    try:
+        resp = await api.ccgen(bin, 10)
+        cards = resp.liveCC
+        await aux.edit(f"""
+**✦ sᴏᴍᴇ ʟɪᴠᴇ ɢᴇɴᴇʀᴀᴛᴇᴅ ᴄᴄ ✦**
+
+๏ `{cards[0]}`\n๏ `{cards[1]}`\n๏ `{cards[2]}`
+๏ `{cards[3]}`\n๏ `{cards[4]}`\n๏ `{cards[5]}`
+๏ `{cards[6]}`\n๏ `{cards[7]}`\n๏ `{cards[8]}`
+๏ `{cards[9]}`
+
+**❅ ʙɪɴ ➠** `{resp.results[0].bin}`
+
+**❅ ᴄᴄ ɢᴇɴ ʙʏ ➠ ˹𝙻𝙾𝙵𝙸 ✗ 𝙼𝚄𝚂𝙸𝙲˼"""
+        )
+    except Exception as e:
+        return await aux.edit(f"**✦ ᴇʀʀᴏʀ ➠** `{e}`")
+
+
---
+++ RYTHMUSIC/plugins/tools/gitinfo.py
@@ -0,0 +1,74 @@
+import asyncio, os, time, aiohttp
+import aiohttp
+from pyrogram import filters
+from blackpink import blackpink as bp
+from DAXXMUSIC import app
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+###
+@app.on_message(filters.command("blackpink"))
+async def blackpink(_, message):
+    text = message.text[len("/blackpink") :]
+    bp(f"{text}").save(f"blackpink_{message.from_user.id}.png")
+    await message.reply_photo(f"blackpink_{message.from_user.id}.png")
+    os.remove(f"blackpink_{message.from_user.id}.png")
+
+
+####
+
+@app.on_message(filters.command(["github", "git"]))
+async def github(_, message):
+    if len(message.command) != 2:
+        await message.reply_text("/git OWNER SE PUCH")
+        return
+
+    username = message.text.split(None, 1)[1]
+    URL = f'https://api.github.com/users/{username}'
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(URL) as request:
+            if request.status == 404:
+                return await message.reply_text("✦ ᴘʟᴇᴀsᴇ ᴘᴜᴛ ᴠᴀʟɪᴅ ɢɪᴛʜᴜʙ ᴜsᴇʀɴᴀᴍᴇ.")
+
+            result = await request.json()
+
+            try:
+                url = result['html_url']
+                name = result['name']
+                company = result['company']
+                bio = result['bio']
+                created_at = result['created_at']
+                avatar_url = result['avatar_url']
+                blog = result['blog']
+                location = result['location']
+                repositories = result['public_repos']
+                followers = result['followers']
+                following = result['following']
+
+                caption = f"""ㅤㅤ✦ ɢɪᴛʜᴜʙ ɪɴғᴏ ᴏғ {name} ✦
+                 
+๏ ᴜsᴇʀɴᴀᴍᴇ ➠ {username}
+๏ ʙɪᴏ ➠ {bio}
+๏ ʟɪɴᴋ ➠ [Here]({url})
+๏ ᴄᴏᴍᴩᴀɴʏ ➠ {company}
+๏ ᴄʀᴇᴀᴛᴇᴅ ᴏɴ ➠ {created_at}
+๏ ʀᴇᴩᴏsɪᴛᴏʀɪᴇs ➠ {repositories}
+๏ ʙʟᴏɢ ➠ {blog}
+๏ ʟᴏᴄᴀᴛɪᴏɴ ➠ {location}
+๏ ғᴏʟʟᴏᴡᴇʀs ➠ {followers}
+๏ ғᴏʟʟᴏᴡɪɴɢ ➠ {following}
+
+๏ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➠ [˹𝙻𝙾𝙵𝙸 ✗ 𝙼𝚄𝚂𝙸𝙲˼](https://t.me/ALONE_xD_MUSIC_BOT)"""
+
+            except Exception as e:
+                print(str(e))
+                pass
+
+    # Create an inline keyboard with a close button
+    close_button = InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close")
+    inline_keyboard = InlineKeyboardMarkup([[close_button]])
+
+    # Send the message with the inline keyboard
+    await message.reply_photo(photo=avatar_url, caption=caption, reply_markup=inline_keyboard)
+
+            
